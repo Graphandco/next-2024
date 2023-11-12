40731671771cc7dc5b5ce1c214cae06317bc4f4d
Graph and Co <contact@graphandco.com>
2023-11-12 14:41:05 +0100

    Add lottie

--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -1,11 +1,15 @@
+import LottieRocket from '@/lottie/LottieRocket';
 import Button from '../UI/Button';
 
 export const HomeHero = () => {
     return (
-        <div className="container py-12">
+        <div className="container py-12 ">
             <Button>
                 <span>Click !</span>
             </Button>
+            <div className="mt-24 mb-[2000px]">
+                <LottieRocket />
+            </div>
         </div>
     );
 };
